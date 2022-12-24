9d4fc3a85f4a26aba6c0d720626c14a37b66226d
colbyfayock <fayock@gmail.com>
2022-12-23 22:15:41 -0500

    title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
       <Container className={styles.headerContainer}>
         <p className={styles.headerTitle}>
           <Link href="/">
-            Cool Clothes
+            Cool Store
           </Link>
         </p>
         <ul className={styles.headerLinks}>
